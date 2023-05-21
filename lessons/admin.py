b216db8ba19945e71bdc56c45d19e32bbf0bcc47
otarza <otar.zakalashvili@gmail.com>
2023-05-21 19:01:00 +0400

    Added models to Django Admin.

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Lesson, LessonCompletion
 
-# Register your models here.
+admin.site.register(Lesson)
+admin.site.register(LessonCompletion)
--- quizzes/admin.py
+++ quizzes/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Quiz, QuizAttempt
 
-# Register your models here.
+admin.site.register(Quiz)
+admin.site.register(QuizAttempt)
